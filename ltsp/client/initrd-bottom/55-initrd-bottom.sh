9f3912db8820a897fd2625efca3b143d74de0334
kvaps <kvapss@gmail.com>
2019-12-03 13:35:17 +0100

    Prepare tmpfs for IMAGE_TO_RAM

--- ltsp/client/initrd-bottom/55-initrd-bottom.sh
+++ ltsp/client/initrd-bottom/55-initrd-bottom.sh
@@ -18,7 +18,8 @@ initrd_bottom_cmdline() {
 }
 
 initrd_bottom_main() {
-    local img_src img rest
+    local img_src img rest tmpfs
+    tmpfs=/run/initramfs/ltsp
 
     warn "Running $0"
     kernel_vars
@@ -32,11 +33,12 @@ initrd_bottom_main() {
         if [ "$IMAGE_TO_RAM" = "1" ]; then
             img=${img_src%%,*}
             rest=${img_src#$img}
-            re mkdir -p "/run/initramfs/ltsp"
-            warn "Running: cp $img /run/initramfs/ltsp/${img##*/}"
-            re cp "$img" "/run/initramfs/ltsp/${img##*/}"
-            re umount "/root"
-            img_src="/run/initramfs/ltsp/${img##*/}$rest"
+            re mkdir -p "$tmpfs/images"
+            re mount -t tmpfs -o mode=0755 tmpfs "$tmpfs/images"
+            warn "Running: cp $img $tmpfs/images/${img##*/}"
+            re cp "$img" "$tmpfs/images/${img##*/}"
+            re umount "$rootmnt"
+            img_src="$tmpfs/images/${img##*/}$rest"
         fi
         re mount_img_src "$img_src" "$rootmnt"
         re set_readahead "$rootmnt"
@@ -58,7 +60,7 @@ initrd_bottom_main() {
     test -d "$rootmnt/proc" || die "$rootmnt/proc doesn't exist in $_APPLET"
     if [ "$OVERLAY" != "0" ]; then
         re modprobe_overlay
-        re overlay "$rootmnt" "$rootmnt" "/run/initramfs/ltsp"
+        re overlay "$rootmnt" "$rootmnt" "$tmpfs/cow"
     fi
     re install_ltsp
 }
