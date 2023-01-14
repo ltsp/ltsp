d16350b4d61909282f73e0c407ce7301ebc69332
Alkis Georgopoulos <alkisg@gmail.com>
2023-01-14 09:31:58 +0200

    Format 55-ipxe.sh

--- ltsp/server/ipxe/55-ipxe.sh
+++ ltsp/server/ipxe/55-ipxe.sh
@@ -1,5 +1,5 @@
 # This file is part of LTSP, https://ltsp.org
-# Copyright 2019 the LTSP team, see AUTHORS
+# Copyright 2019-2023 the LTSP team, see AUTHORS
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Install iPXE binaries and configuration in TFTP
@@ -14,9 +14,15 @@ ipxe_cmdline() {
     eval "set -- $args"
     while true; do
         case "$1" in
-            -b|--binaries) shift; BINARIES=${1:-1} ;;
-            --) shift; break ;;
-            *) die "ltsp $_APPLET: error in cmdline: $*" ;;
+        -b | --binaries)
+            shift
+            BINARIES=${1:-1}
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *) die "ltsp $_APPLET: error in cmdline: $*" ;;
         esac
         shift
     done
@@ -35,10 +41,10 @@ ipxe_main() {
     img_name=$(re list_img_names -i)
     set -- $img_name
     for img_name in "$@"; do
-        key=$((key+1))
+        key=$((key + 1))
         title=$(echo_values "$(ipxe_name "$img_name.img")")
         title=${title:-$img_name.img}
-        items="${items:+"$items\n"}$(printf "item --key %d %-20s %s" "$((key%10))" "$img_name" "$title")"
+        items="${items:+"$items\n"}$(printf "item --key %d %-20s %s" "$((key % 10))" "$img_name" "$title")"
         gotos=":$img_name\n$gotos"
     done
     r_items=""
@@ -46,10 +52,10 @@ ipxe_main() {
     img_name=$(re list_img_names -c)
     set -- $img_name
     for img_name in "$@"; do
-        key=$((key+1))
+        key=$((key + 1))
         title=$(echo_values "$(ipxe_name "$img_name")")
         title=${title:-$img_name}
-        r_items="${r_items:+"$r_items\n"}$(printf "item --key %d %-20s %s" "$((key%10))" "r_$img_name" "$title")"
+        r_items="${r_items:+"$r_items\n"}$(printf "item --key %d %-20s %s" "$((key % 10))" "r_$img_name" "$title")"
         r_gotos=":r_$img_name\nset img $img_name \&\& goto roots\n$r_gotos"
     done
     re mkdir -p "$TFTP_DIR/ltsp"
@@ -79,10 +85,10 @@ s|^:roots\$|$(textif "$r_items" "$r_gotos" "&")|
                 elif [ -f "/usr/lib/ipxe/$binary" ]; then
                     binsrc="/usr/lib/ipxe/$binary"
                 elif [ "$binary" = "memtest.0" ] &&
-                        [ -f "/boot/memtest86+.bin" ]; then
+                    [ -f "/boot/memtest86+.bin" ]; then
                     binsrc="/boot/memtest86+.bin"
                 elif [ "$binary" = "snponly.efi" ] &&
-                        [ -f "/usr/lib/ipxe/ipxe.efi" ]; then
+                    [ -f "/usr/lib/ipxe/ipxe.efi" ]; then
                     binsrc="/usr/lib/ipxe/ipxe.efi"
                 elif [ "${binary%.*}" = "memtest" ]; then
                     warn "$binary not found, that iPXE menu won't work"
@@ -109,30 +115,30 @@ client_sections() {
     for section in $(section_list); do
         # We only care about mac address sections
         case "$section" in
-            section_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f])
-                mac=$(echo "$section" |sed 's/section_//;s/_/:/g')
-                # Use a subshell to avoid overriding useful variables
-                (
-                    unset DEFAULT_IMAGE KERNEL_PARAMETERS MENU_TIMEOUT
-                    unset HOSTNAME
-                    section_call "$mac"
-                    test -n "$DEFAULT_IMAGE$KERNEL_PARAMETERS$MENU_TIMEOUT" ||
-                        return 1
-                    # Print an empty line between sections
-                    test "$first" = "1" || printf '\\n\\n'
-                    printf ':%s' "$mac"
-                    test -n "$HOSTNAME" &&
-                        printf '\\nset hostname %s' "$HOSTNAME"
-                    test -n "$DEFAULT_IMAGE" &&
-                        printf '\\nset img %s' "$DEFAULT_IMAGE"
-                    test -n "$KERNEL_PARAMETERS" &&
-                        printf '\\nset cmdline_client %s' "$KERNEL_PARAMETERS"
-                    test -n "$MENU_TIMEOUT" &&
-                        printf '\\nset menu-timeout %s' "$MENU_TIMEOUT"
-                    printf '\\ngoto start'
-                ) || continue
-                unset first
-                ;;
+        section_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f]_[0-9a-f][0-9a-f])
+            mac=$(echo "$section" | sed 's/section_//;s/_/:/g')
+            # Use a subshell to avoid overriding useful variables
+            (
+                unset DEFAULT_IMAGE KERNEL_PARAMETERS MENU_TIMEOUT
+                unset HOSTNAME
+                section_call "$mac"
+                test -n "$DEFAULT_IMAGE$KERNEL_PARAMETERS$MENU_TIMEOUT" ||
+                    return 1
+                # Print an empty line between sections
+                test "$first" = "1" || printf '\\n\\n'
+                printf ':%s' "$mac"
+                test -n "$HOSTNAME" &&
+                    printf '\\nset hostname %s' "$HOSTNAME"
+                test -n "$DEFAULT_IMAGE" &&
+                    printf '\\nset img %s' "$DEFAULT_IMAGE"
+                test -n "$KERNEL_PARAMETERS" &&
+                    printf '\\nset cmdline_client %s' "$KERNEL_PARAMETERS"
+                test -n "$MENU_TIMEOUT" &&
+                    printf '\\nset menu-timeout %s' "$MENU_TIMEOUT"
+                printf '\\ngoto start'
+            ) || continue
+            unset first
+            ;;
         esac
     done
 }
@@ -141,4 +147,3 @@ ipxe_name() {
     echo "$*" |
         awk '{ var=toupper($0); gsub("[^A-Z0-9]", "_", var); print "IPXE_" var }'
 }
-
